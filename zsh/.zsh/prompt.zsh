11e2c6acd9bae309eb09400b55b407a45c263183
Charlotte Som <charlotte@som.codes>
2026-01-28 22:19:15 +0000

    improve jj vcs info prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -7,6 +7,11 @@ _prompt_color() {
 }
 
 function _prompt_parse_git_dirty() {
+  if jj status 2>&1 > /dev/null; then
+    echo ""
+    return
+  fi
+
   local STATUS=''
   STATUS=$(command git status --porcelain --ignore-submodules=dirty 2> /dev/null | tail -n1)
 
@@ -22,8 +27,6 @@ _prompt_pipenv() {
     echo " [rye]"
   elif [[ -n $PIPENV_ACTIVE ]]; then
     echo " [pipenv]"
-  elif [[ -n $VIRTUAL_ENV_PROMPT ]]; then
-    echo "$VIRTUAL_ENV_PROMPT" | sed 's/^(\(.*\)) $/ [\1]/'
   else
     echo ""
   fi
@@ -34,15 +37,18 @@ prompt_precmd() {
     vcs_info
 
     if [[ -z ${vcs_info_msg_0_} ]]; then
-      PROMPT="$(_prompt_color)%3/%f$(_prompt_pipenv) $ "
+      PROMPT="$(_prompt_color)%3/%f"
     else
       PROMPT="$(_prompt_color)%3/%f ("
       PROMPT+='${vcs_info_msg_0_}'
-      PROMPT+="$(_prompt_parse_git_dirty))$(_prompt_pipenv) $ "
+      PROMPT+="$(_prompt_parse_git_dirty))"
     fi
   else
-    PROMPT="$(_prompt_color)%3/%f$(_prompt_pipenv) $ "
+    PROMPT="$(_prompt_color)%3/%f"
   fi
+
+  PROMPT+="$(_prompt_pipenv)"
+  PROMPT+=" $ "
 
   RPROMPT="%F{grey}%F{7}%n@%M%f"
 }
@@ -56,11 +62,11 @@ prompt_init() {
 
   add-zsh-hook precmd prompt_precmd
 
-  zstyle ':vcs_info:*' enable git jj
+  zstyle ':vcs_info:*' enable jj git
   zstyle ':vcs_info:*' use-simple true
   zstyle ':vcs_info:git:*' formats '%b'
   zstyle ':vcs_info:git:*' actionformats '%b|%a'
-  zstyle ':vcs_info:jj:*' formats '%b %i'
+  zstyle ':vcs_info:jj:*' formats '%s: %i'
 
   zstyle ':vcs_info:*' max-exports 2
 }
