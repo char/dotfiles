2442409842e3b5b08110ddea0378c2d042d56876
Charlotte Som <charlotte@som.codes>
2026-03-10 18:35:10 +0000

    zsh: use bun if it exists
    sigh..

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -2,8 +2,17 @@
 NPM_PACKAGES="${HOME}/.npm-packages"
 export PATH="$NPM_PACKAGES/bin:$PATH"
 
-export DENO_HOME="$HOME/.deno"
-export PATH="$DENO_HOME/bin:$PATH"
+if [ -s "$HOME/.deno" ]; then
+  export DENO_HOME="$HOME/.deno"
+  export PATH="$DENO_HOME/bin:$PATH"
+fi
+
+if [ -s "$HOME/.bun" ]; then
+  export BUN_HOME="$HOME/.bun"
+  export PATH="$BUN_HOME/bin:$PATH"
+  source "$BUN_HOME/_bun"
+fi
+
 
 if [ -f "/usr/bin/opam" ]
 then
